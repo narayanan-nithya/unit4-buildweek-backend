f31514593b833efcb42b6ce00724add5b50a2ef9
Nithya Narayanan <n.nithya@pm.me>
2022-03-03 18:45:27 -0800

    created user-model

---
+++ api/users/user-model.js
@@ -0,0 +1,43 @@
+const db = require('../data/db-config')
+
+async function get() {
+    return await db('users')
+}
+
+async function getBy(filter) {
+    return await db('users').where(filter)
+}
+
+async function getById(id) {
+    return await db('users').where('user_id', id)
+}
+
+async function getPlantByUserId(id) {
+    const userPlants = await db('users as u')
+        .leftJoin('plants as p', 'u.user_id', 'p.user_id')
+        .select('p.*')
+        .where('p.user_id', id)
+    return {
+        userPlants
+    }
+}
+
+function update(id, changes){
+    return db("users")
+    .where("user_id", id)
+    .update(changes)
+}
+
+async function create(newUser){
+    await db('users').insert(newUser)
+    return newUser
+}
+
+module.exports ={
+    get, 
+    getBy,
+    getById, 
+    getPlantByUserId,
+    update, 
+    create
+}
